386b9a83b82fe8aff2712e68b84c802f5dd83219
João Alexandre Lucena Ferrão <joaoferrao77@gmail.com>
2023-02-24 17:00:32 -0300

    adicionado layout do form de inclusão de produtos

--- src/views/AddProdutoView.vue
+++ src/views/AddProdutoView.vue
@@ -1,22 +1,78 @@
-<script setup>
-</script>
+
 
 <template>
-  <main>
-    <h2>Manutenção de Produtos</h2>
-    <div class="search-bar">
-        <RouterLink to="/add-produto">Adicionar</RouterLink>
-        <span>Pesquisar <input type="text"></span>
-    </div>
-  </main>
+    <main>
+        <h2>Inclusao de Produtos</h2>
+        <form>
+            <table>
+                <tr>
+                    <td>ID</td>
+                    <td><input type="text" name="id" readonly></td>
+                </tr>
+                <tr>
+                    <td>SKU</td>
+                    <td><input type="text" name="sku"></td>
+                </tr>
+                <tr>
+                    <td>Descricao</td>
+                    <td><input type="text" name="descricao"></td>
+                </tr>
+                <tr>
+                    <td>Data Referencia</td>
+                    <td><input type="date" name="data_referencia"></td>
+                </tr>
+                <tr>
+                    <td>Valor Referencia</td>
+                    <td><input type="text" name="valor_referencia"><br>Valor devera ser superior a 1.00</td>
+                </tr>
+            </table>
+            <div id="button-bar">
+                <button><RouterLink to="/produto">Voltar</RouterLink></button>
+                <button>Gravar</button>
+            </div>
+        </form>
+    </main>
 </template>
 
+<script>
+import api from "@/services/api";
+
+export default {
+    name: 'AddProdutoView',
+    data() {
+        return {
+        }
+    },
+}
+</script>
+
 <style scoped>
-.search-bar {
+main {
+    width: 100%;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+form {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+table, td {
+    border: 1px solid black;
+    text-align: start;
+    padding: 10px;
+}
+
+#button-bar {
     width: 100%;
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
+    justify-content: flex-end;
+    gap: 15px;
 }
 </style>
