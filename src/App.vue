66b871c4a7a669afca9a78d141dc6265303632a9
João Alexandre Lucena Ferrão <joaoferrao77@gmail.com>
2023-02-24 16:59:41 -0300

    adicionado rodapé

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { RouterLink, RouterView } from 'vue-router'
 import Topbar from './components/Topbar.vue'
 import Sidebar from './components/Sidebar.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
@@ -10,11 +11,13 @@ import Sidebar from './components/Sidebar.vue'
     <Sidebar></Sidebar>
     <RouterView />
   </div>
+  <Footer></Footer>
 </template>
 
 <style scoped>
 .main-content {
   width: 100%;
+  height: inherit;
   display: flex;
   flex-direction: row;
 }
---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <div class="footer">
+    <h4>Copyright &#169; 2021</h4>
+  </div>
+</template>
+
+<style scoped>
+  .footer {
+    width: 100%;
+    bottom: 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    background-color: #c3c3c3;
+    padding: 10px;
+  }
+</style>
