293c35895480c522b9f4b49cd1785fd1ee864831
João Alexandre Lucena Ferrão <joaoferrao77@gmail.com>
2023-02-24 14:35:10 -0300

    adicionado Sidebar & Topbar + alteração de estrutura do projeto + removido arquivos inúteis

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <div class="sidebar">
+    <h3>Menu Opcoes</h3>
+    <hr/>
+    <RouterLink to="/produto">Produto</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+.sidebar {
+  background-color: #c3c3c3;
+  width: 150px;
+  min-height: -webkit-fill-available;
+  padding: 10px;
+}
+</style>
---
+++ src/components/Topbar.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+
+<template>
+  <div class="topbar">
+    <img src="../assets/logo_turion.png" alt="Logomarca da Turion">
+    <h1>TECIDOS DIVERSOS LTDA</h1>
+  </div>
+</template>
+
+<style scoped>
+  .topbar {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 10px;
+
+    background-color: #c3c3c3;
+    padding: 10px;
+  }
+
+  .topbar img {
+    max-width: 50px;
+    height: fit-content;
+  }
+
+  .topbar h1 {
+    font-size: 20px;
+  }
+</style>
