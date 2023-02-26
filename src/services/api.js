2b9d2dfdfd9b0759fb22468999c4d6c87a199408
João Alexandre Lucena Ferrão <joaoferrao77@gmail.com>
2023-02-26 17:32:53 -0300

    adicionado header content type application/json no axios

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "http://177.85.34.62:63616/",
+    headers: {
+        "Content-Type": "application/json",
+    }
 })
 
 export default api;
